4f0012b3fb5157c0091409971a4b3ff93e1116d3
aarontkrieg <56848457+aarontkrieg@users.noreply.github.com>
2021-07-15 23:05:07 -0700

    removed example users from static database

--- server.js
+++ server.js
@@ -2,24 +2,11 @@ const express = require('express')
 
 const app = express();
 
+app.use(express.json());
+
 const database = {
 	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally@gmail.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
+
 	]
 }
 
