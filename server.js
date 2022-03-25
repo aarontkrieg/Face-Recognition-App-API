df0b51cb8b9bdb943fbf2eee3a2163dd472ea1af
aarontkrieg <56848457+aarontkrieg@users.noreply.github.com>
2022-03-24 19:07:55 -0700

    added database functionality to profile id API and returns proper erroring

--- server.js
+++ server.js
@@ -66,16 +66,16 @@ app.post('/signin', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		} 
-	})
-	if (!found) {
-		res.status(400).json('not found');
+	db.select('*').from('users').where({id})
+	.then(user => {
+		console.log(user)
+		if (user.length) {
+			res.json(user[0])
+		} else {
+		res.status(400).json('Not Found')
 	}
+	})
+	.catch(err => res.status(400).json('error getting user'))
 })
 
 app.post('/register', (req, res) => {
