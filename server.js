94ca4f467d542fa8fe5ac6e821c5527d17851e9d
aarontkrieg <56848457+aarontkrieg@users.noreply.github.com>
2022-03-25 12:14:27 -0700

    added functionality to signin api and corrected bcrypt hash error

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
-const app = express();
 const cors = require('cors');
 const knex = require('knex')
 
@@ -14,9 +13,11 @@ const db = knex({
   }
 });
 
-db.select('*').from('users').then(data => {
-	console.log(data);;
-});
+const app = express();
+
+// db.select('*').from('users').then(data => {
+// 	console.log(data);;
+// });
 
 app.use(express.json());
 app.use(cors());
@@ -51,32 +52,32 @@ app.use(cors());
 // }
 
 app.get('/', (req, res) => {
-	res.send(database.users);
+	res.send('success')
 })
 
 app.post('/signin', (req, res) => {
-	db.select('email', 'hash').from('login')
-		.where('email', '=', req.body.email)
-		.then(data => {
-			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-			if (isValid) {
-				return db.select('*').from('users')
-				.where('email', '=', req.body.email)
-				.then(user => {
-					res.json(user[0])
-				})
-			.catch(err => res.status(400).json('unable to get user'))
-		} else {
-			(err => res.status(400).json('wrong credentials'))
-		}
-	})
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
 	db.select('*').from('users').where({id})
 	.then(user => {
-		console.log(user)
 		if (user.length) {
 			res.json(user[0])
 		} else {
